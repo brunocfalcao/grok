f4c2f70713d56fc9f88f46145dd062e29dc46c59
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-17 00:06:19 +0100

    wip

--- src/GrokServiceProvider.php
+++ src/GrokServiceProvider.php
@@ -2,11 +2,32 @@
 
 namespace Nidavellir\Grok;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class GrokServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->loadRoutes();
+
+        $this->loadViewsFrom(__DIR__.'/../views', 'grok');
+    }
+
+    protected function loadRoutes()
+    {
+        Route::middleware([
+            'api',
+        ])
+        ->group(function () {
+            include __DIR__ . '/../routes/api.php';
+        });
+
+        Route::middleware([
+            'web',
+        ])
+        ->group(function () {
+            include __DIR__ . '/../routes/web.php';
+        });
     }
 }
