d76c44802f2a50832c96d8f22ca40175c0c9fb2b
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-27 19:11:08 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use Nidavellir\Thor\Models\System;
 use Illuminate\Support\Facades\Route;
 use Nidavellir\Grok\Controllers\AccountDashboardController;
 
 Route::get('/', function () {
     return view('grok::welcome');
+});
+
+Route::get('/stop', function () {
+    System::update(['can_process_scheduled_tasks' => false]);
+    return 'Scheduled tasks stopped. Bot is down.';
 });
 
 Route::get('/accounts/{uuid}/dashboard', [AccountDashboardController::class, 'show']);
