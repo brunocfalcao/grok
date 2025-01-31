c775db4021da8978e22d1c0a8bc5599cdeb485d2
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-31 21:08:23 +0100

    wip

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ use Nidavellir\Thor\Models\Account;
 
 // Add the route with throttling middleware
 Route::middleware([
-    'throttle:60,1', // Throttles requests to 60 per minute
+    'throttle:60,1', // Throttles requests to 10 per minute
 ])->group(function () {
-    Route::get('api/account/positions/{uuid}', function (Request $request, $uuid) {
+    Route::get('api/account/{uuid}/positions', function (Request $request, $uuid) {
         // Log the request for audit
         Log::info('Fetching dashboard information for account', ['uuid' => $uuid, 'ip' => $request->ip()]);
 
