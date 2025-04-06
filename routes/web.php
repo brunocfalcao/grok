a3d1e20d2e6745cf3e8c18689ced29303553e342
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-04-06 11:44:40 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-use Nidavellir\Grok\Controllers\AccountDashboardController;
-use Nidavellir\Thor\Models\System;
-use Nidavellir\Thor\Models\User;
 
 Route::get('/', function () {
     return view('grok::welcome');
